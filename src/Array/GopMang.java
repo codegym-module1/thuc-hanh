84a2008a5bb981abe4fd450b2b42eabd0266d85d
Son Nguyen <vietson6103@gmail.com>
2018-07-08 16:17:41 +0700

    Thuc hanh gop mang

---
+++ src/Array/GopMang.java
@@ -0,0 +1,10 @@
+package Array;
+import java.util.Scanner;
+
+public class GopMang {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array1 = {2, 5, 6, 7};
+        int[] array2 = {3, 5, 6, 7};
+    }
+}
